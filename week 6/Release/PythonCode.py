c50d27645f581dce4d9df63e398cdbc3f466b52c
Its-Mocha <93355721+Its-Mocha@users.noreply.github.com>
2021-12-15 09:43:46 -0500

    Add files via upload
    Adding a Assignment for testing/ viewing purposes. This assignment we used both C++ and Python to make a application that prints out a simple menu and takes a users number and gives out a 1-10 multination table or a double value  for said input.  12/15/2021

---
+++ week 6/Release/PythonCode.py
@@ -0,0 +1,28 @@
+import re
+import string
+
+
+def MultiplicationTable(number):
+    i = 1
+    while i <= 10:
+       print ( i, "X", number, "=", i * number)
+       i+=1
+    return 0
+       
+
+def DoubleValue (v):       
+    return 2 * v
+
+def printsomething():
+    print("Hello from python!")
+
+def PrintMe(v):
+    print("You sent me: " + v)
+    return 1001;
+
+def SquareValue(v):
+    return v * v
+
+
+
+    
